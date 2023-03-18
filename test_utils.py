0911ca79c21bc3b630607857aebeb48841f27a22
Galaev1 <Amin29_07@mail.ru>
2023-03-19 00:07:25 +0300

    term paper 2.0

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,5 @@
-from utils import  get_data
+import pytest
+from utils import get_data
 
 def test_get_data():
     url = 'https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678990788899&signature=T5o3zc1S8Whs4ihvCoRYOm3p0uzScj6Hm3_VOx6t-Qs&downloadName=operations.json'
